faaa701e930b019369e4d7a562dc29b7f475f9b9
Zhicun08 <zhicunwang@newschool.edu>
2022-04-06 11:35:18 +0800

    elevators work separately

--- Battle Royale Group/Assets/Scripts/ElevatorTrigger.cs
+++ Battle Royale Group/Assets/Scripts/ElevatorTrigger.cs
@@ -7,18 +7,39 @@ public class ElevatorTrigger : MonoBehaviour
     public GameObject elevator;
     public float distance;
 
-    //elevatorTrigger
+    public List<Transform> points;
+    public float moveSpeed;
+    public int currentPoint;
+
     public bool slimeOn;
-    public GameObject slime;
-    public LayerMask platformTriggerLayer;
+    public bool dancerOn;
+    public bool rabbitOn;
 
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector2 (elevator.transform.position.x, elevator.transform.position.y+distance);
-        
-        //elevatorTrigger
-        slimeOn = Physics2D.OverlapCircle(slime.transform.position, .3f, platformTriggerLayer);
+        if( slimeOn==true && dancerOn == true ){
+            elevator2();
+        }
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Slime")){
+            slimeOn = true;
+        }
+        if(other.gameObject.CompareTag("Dancer")){
+            dancerOn = true;
+        }
+    }
+
+
+
+    void elevator2(){
+        // print(platform.position);
+        // Move from the current position to the next point
+        elevator.transform.position = Vector2.MoveTowards(elevator.transform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
     }
 }
--- Battle Royale Group/Assets/Scripts/waterTrans.cs
+++ Battle Royale Group/Assets/Scripts/waterTrans.cs
@@ -8,7 +8,7 @@ public class waterTrans : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("ice_bullet")){
-            print("!!!!");
+            //print("!!!!");
             gameObject.SetActive(false);
             Destroy(other.gameObject);
             icePlatform.gameObject.SetActive(true);
