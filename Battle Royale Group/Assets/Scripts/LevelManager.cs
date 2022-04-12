f8302fc9cc4d429996d5669de923798ccc71aca6
Linh Le <mlinhle617@gmail.com>
2022-04-11 23:30:06 -0400

    Updated level manager script
    Made it so that if one player runs out of health, everyone has to restart the level

--- Battle Royale Group/Assets/Scripts/LevelManager.cs
+++ Battle Royale Group/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -24,7 +25,17 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if any player runs out of lives
+        for (int i = 0; i < healthControllers.Length; i++)
+        {
+            // If one player runs out of lives
+            if (healthControllers[i].currentLives <= 0)
+            {
+                // Restart the level
+                StartCoroutine(RestartLevel());
+            }
+            
+        }
     }
 
     public void RespawnPlayer()
@@ -47,5 +58,11 @@ public class LevelManager : MonoBehaviour
             healthControllers[i].gameObject.SetActive(true);
             healthControllers[i].gameObject.transform.position = CheckpointController.instance.spawnPoint;
         }
+    }
+
+    IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(waitToRespawn);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
