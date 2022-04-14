4b1d6c2ca57f8c332b0342d36cf6d1cbd825de3a
Linh Le <mlinhle617@gmail.com>
2022-04-13 20:20:27 -0400

    Updated detect collision script

--- Battle Royale Group/Assets/Scripts/SpaceRabbit Scripts/DetectCollision.cs
+++ Battle Royale Group/Assets/Scripts/SpaceRabbit Scripts/DetectCollision.cs
@@ -7,18 +7,31 @@ public class DetectCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
+            if (other.CompareTag("Enemy") && gameObject.CompareTag("Bullet"))
+            {
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            }
+
+        if (other.CompareTag("Plant") && gameObject.CompareTag("fire_bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Swan") && gameObject.CompareTag("Shoe"))
         {
             Destroy(other.gameObject);
             Destroy(gameObject);
